dd754b528d563b219a145ac8d39e687d00e068dc
Johiny <johiny7@live.com>
2021-10-20 16:02:17 -0500

    instlación de express y nodemon

--- server.js
+++ server.js
@@ -2,4 +2,19 @@ import  Express  from "express";
 
 const app = Express();
 
+app.get("/Ventas", (req,res) => {
+
+    const vehiculos = [] // traer de la BD
+
+    console.log("¿quieres las ventas?")
+    res.send("No hay ventas sorry")
+})
+
+
+app.post("/Ventas/Newventa",(req,res) => {
+
+    //enviar vehiculo a la base de datos
+    res.send("venta guardada correctamente!")
+    console.log("procesando venta")
+})
 app.listen(5000,() => {console.log("estoy escuchando desde el backend!")})
