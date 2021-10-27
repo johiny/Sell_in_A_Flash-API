fff6adeb7318b423896309477d9111ab81d7555a
Juan Esteban Orozco <jeorozcob@gmail.com>
2021-10-27 15:02:10 -0500

    jwt check

--- server.js
+++ server.js
@@ -4,13 +4,34 @@ import dotenv from "dotenv"
 import {bdConnection} from "./db/db.js"
 import rutasVentas from "./views/Ventas/rutas.js"
 dotenv.config({path:"./.env"})
+import jwt from 'express-jwt';
+import jwks from 'jwks-rsa';
+
+
 
 //Se crean las variables y objetos para poder implementar express y mongodb
-const app = Express();
-
-app.use(Express.json());
+const express = require('express');
+const app = express();
+const { auth } = require('express-oauth2-jwt-bearer');
+app.use(express.json());
 app.use(Cors());
 app.use(rutasVentas);
+
+var jwtCheck = jwt({
+    secret: jwks.expressJwtSecret({
+        cache: true,
+        rateLimit: true,
+        jwksRequestsPerMinute: 5,
+        jwksUri: 'https://dev-0skfin92.us.auth0.com/.well-known/jwks.json'
+  }),
+  audience: 'api-autenticacion-sellinaflash',
+  issuer: 'https://dev-0skfin92.us.auth0.com/',
+  algorithms: ['RS256']
+}); 
+
+app.use(jwtCheck);
+
+
 
 // inicia el servidor, si se conecta a la base de datos envia un mensaje en la terminal y se pone a escuchar un puerto, de otra manera lanza error
 
