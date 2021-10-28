cf1f94cdf52a49bd30f9984b71df5b14a720ff9f
Juan Esteban Orozco <jeorozcob@gmail.com>
2021-10-27 21:33:01 -0500

    no entiendo por qué se deshicieron los cambios en el back :(

--- controllers/usuarios/controller.js
+++ controllers/usuarios/controller.js
@@ -3,36 +3,37 @@ import { getDB } from '../../db/db.js';
 import jwt_decode from 'jwt-decode';
 
 const queryAllUsers = async (callback) => {
-  const connection = getDB();
+  const baseDeDatos = getDB();
   console.log('query');
-  await connection.collection('usuario').find({}).limit(50).toArray(callback);
+  await baseDeDatos.collection('usuario').find({}).limit(50).toArray(callback);
 };
 
 const crearUsuario = async (datosUsuario, callback) => {
-  const connection = getDB();
-  await connection.collection('usuario').insertOne(datosUsuario, callback);
+  const baseDeDatos = getDB();
+  await baseDeDatos.collection('usuario').insertOne(datosUsuario, callback);
 };
 
 const consultarUsuario = async (id, callback) => {
-  const connection = getDB();
-  await connection.collection('usuario').findOne({ _id: new ObjectId(id) }, callback);
+  const baseDeDatos = getDB();
+  await baseDeDatos.collection('usuario').findOne({ _id: new ObjectId(id) }, callback);
 };
 
 const consultarOCrearUsuario = async (req, callback) => {
-
+  console.log('Estoy llegando a crear usuario');
+  // 6.1. obtener los datos del usuario desde el token
   const token = req.headers.authorization.split('Bearer ')[1];
   const user = jwt_decode(token)['http://localhost/userData'];
   console.log(user);
 
-
-  const connection = getDB();
-  await connection.collection('usuario').findOne({ email: user.email }, async (err, response) => {
+  // 6.2. con el correo del usuario o con el id de auth0, verificar si el usuario ya esta en la bd o no
+  const baseDeDatos = getDB();
+  await baseDeDatos.collection('usuario').findOne({ email: user.email }, async (err, response) => {
     console.log('response consulta bd', response);
     if (response) {
-
+      // 7.1. si el usuario ya esta en la BD, devuelve la info del usuario
       callback(err, response);
     } else {
-
+      // 7.2. si el usuario no esta en la bd, lo crea y devuelve la info
       user.auth0ID = user._id;
       delete user._id;
       user.rol = 'sin rol';
@@ -47,16 +48,16 @@ const editarUsuario = async (id, edicion, callback) => {
   const operacion = {
     $set: edicion,
   };
-  const connection = getDB();
-  await connection
+  const baseDeDatos = getDB();
+  await baseDeDatos
     .collection('usuario')
     .findOneAndUpdate(filtroUsuario, operacion, { upsert: true, returnOriginal: true }, callback);
 };
 
 const eliminarUsuario = async (id, callback) => {
   const filtroUsuario = { _id: new ObjectId(id) };
-  const connection = getDB();
-  await connection.collection('usuario').deleteOne(filtroUsuario, callback);
+  const baseDeDatos = getDB();
+  await baseDeDatos.collection('usuario').deleteOne(filtroUsuario, callback);
 };
 
 export {
