703a62f5a898e4362f7fbbd674f90bebdb710cdc
Juan Esteban Orozco <jeorozcob@gmail.com>
2021-10-28 13:26:56 -0500

    comentarios

--- controllers/usuarios/controller.js
+++ controllers/usuarios/controller.js
@@ -20,20 +20,17 @@ const consultarUsuario = async (id, callback) => {
 
 const consultarOCrearUsuario = async (req, callback) => {
   console.log('Estoy llegando a crear usuario');
-  // 6.1. obtener los datos del usuario desde el token
   const token = req.headers.authorization.split('Bearer ')[1];
   const user = jwt_decode(token)['http://localhost/userData'];
   console.log(user);
 
-  // 6.2. con el correo del usuario o con el id de auth0, verificar si el usuario ya esta en la bd o no
+
   const SFBD = getDB();
   await SFBD.collection('usuario').findOne({ email: user.email }, async (err, response) => {
     console.log('response consulta bd', response);
     if (response) {
-      // 7.1. si el usuario ya esta en la BD, devuelve la info del usuario
       callback(err, response);
     } else {
-      // 7.2. si el usuario no esta en la bd, lo crea y devuelve la info
       user.auth0ID = user._id;
       delete user._id;
       user.rol = 'sin rol';
